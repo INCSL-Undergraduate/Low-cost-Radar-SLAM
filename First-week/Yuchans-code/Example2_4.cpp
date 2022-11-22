b5f1193a986b6b776a6f31b0421b6dca0a4f4ab7
uchan21 <81511939+uchan21@users.noreply.github.com>
2022-11-22 18:03:25 +0900

    Add files via upload

---
+++ First-week/Yuchans-code/Example2_4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+    cout<<"input your name>>";
+    char name[11];
+    cin >> name;
+    cout << "name is "<<name<<endl;
+}
---
+++ First-week/Yuchans-code/Example2_6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"input your address>>";
+    char address[100];
+    cin.getline(address,100,'\n');
+
+    cout<<"Address is "<<address<<endl;
+}
